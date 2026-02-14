7fa24c8eb187d4afe09d42297bcb5544d4d55d35
rumeysa1017 <rumeysa1017@gmail.com>
2026-02-14 14:23:24 +0100

    feat: secure slack notifications using secrets

--- dq_pipeline.py
+++ dq_pipeline.py
@@ -1,20 +1,32 @@
 import pandas as pd
 import great_expectations as gx
 import sys
+import requests
+import os
+
+def send_slack_notification(message):
+    # GitHub Secrets'tan gelen URL'i okur
+    webhook_url = os.getenv("SLACK_WEBHOOK_URL")
+    if webhook_url:
+        payload = {"text": message}
+        try:
+            requests.post(webhook_url, json=payload)
+        except Exception as e:
+            print(f"Slack gönderim hatası: {e}")
 
 def run_validation():
     # 1. Veriyi Yükle
     df = pd.read_csv("data/amazon_orders.csv", low_memory=False)
     
-    # 2. GE Context ve Suite Ayarları
+    # 2. GE Ayarları
     context = gx.get_context()
     suite = context.suites.add(gx.ExpectationSuite(name="ci_suite"))
     
-    # 3. Beklentileri Ekle (Basit tutalım CI hızlı çalışsın)
+    # Beklentiler
     suite.add_expectation(gx.expectations.ExpectColumnValuesToNotBeNull(column="Order ID"))
     suite.add_expectation(gx.expectations.ExpectColumnValuesToBeBetween(column="Qty", min_value=0))
 
-    # 4. Datasource ve Batch Tanımı
+    # 3. Validation Tanımı
     data_source = context.data_sources.add_pandas(name="my_datasource")
     data_asset = data_source.add_dataframe_asset(name="my_asset")
     batch_definition = data_asset.add_batch_definition_whole_dataframe("my_batch")
@@ -25,16 +37,24 @@ def run_validation():
         name="ci_validation"
     )
 
-    # 5. Çalıştır
+    # 4. Çalıştır
     results = validation_definition.run(batch_parameters={"dataframe": df})
 
-    # 6. SONUÇ KONTROLÜ (CI için en kritik yer)
+    # 5. Özet ve Slack Bildirimi
+    # GitHub'ın bize sağladığı otomatik değişkenleri kullanalım
+    repo_name = os.getenv('GITHUB_REPOSITORY', 'Unknown Repo')
+    run_id = os.getenv('GITHUB_RUN_ID', '0')
+    job_url = f"https://github.com/{repo_name}/actions/runs/{run_id}"
+
     if not results.success:
-        print("❌ Veri Kalitesi Doğrulaması Başarısız!")
-        # GitHub'a "Hata var!" mesajı gönderiyoruz
-        sys.exit(1) 
+        summary = f"🚨 *DQ ALERT: Validation Failed!* 🚨\n\n*Repo:* {repo_name}\n*Detaylar:* <{job_url}|GitHub Actions Loglarını Görüntüle>"
+        send_slack_notification(summary)
+        print("❌ Hata Slack'e bildirildi.")
+        sys.exit(1)
     else:
-        print("✅ Veri Kalitesi Doğrulaması Başarılı.")
+        # Başarılı olduğunda da kısa bir not istersen:
+        # send_slack_notification(f"✅ DQ Passed: {repo_name}")
+        print("✅ Her şey yolunda.")
         sys.exit(0)
 
 if __name__ == "__main__":
